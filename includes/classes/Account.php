d5fb6df223c1c4362e6e2315e9340f886652f4c5
slasherdavidkun <slasherdavidkun@gmail.com>
2020-05-05 01:16:38 +0800

    validate password

--- includes/classes/Account.php
+++ includes/classes/Account.php
@@ -27,19 +27,51 @@
 		}
 
 		private function validateFirstName($fn) {
-			
+			if(strlen($fn) > 25 || strlen($fn) < 2) {
+				array_push($this->errorArray, "Your first name must be between 2 and 25 characters");
+				return;
+			}
 		}
 
 		private function validateLastName($ln) {
-			
+			if(strlen($ln) > 25 || strlen($ln) < 2) {
+				array_push($this->errorArray, "Your last name must be between 2 and 25 characters");
+				return;
+			}
 		}
 
 		private function validateEmails($em, $em2) {
-			
+			if($em != $em2) {
+				array_push($this->errorArray, "Your emails don't match");
+				return;
+			}
+
+			if(!filter_var($em, FILTER_VALIDATE_EMAIL)) {
+				array_push($this->errorArray, "Email is invalid");
+				return;
+			}
+
+			//TODO: Check that username hasn't already been used
+
 		}
 
 		private function validatePasswords($pw, $pw2) {
 			
+			if($pw != $pw2) {
+				array_push($this->errorArray, "Your passwords don't match");
+				return;
+			}
+
+			if(preg_match('/[^A-Za-z0-9]/', $pw)) {
+				array_push($this->errorArray, "Your password can only contain numbers and letters");
+				return;
+			}
+
+			if(strlen($pw) > 30 || strlen($pw) < 5) {
+				array_push($this->errorArray, "Your password must be between 5 and 30 characters");
+				return;
+			}
+
 		}
 
 
