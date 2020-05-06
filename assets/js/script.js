7ab185396a60863ce0c941adffea44ec641edf90
slasherdavidkun <slasherdavidkun@gmail.com>
2020-05-06 18:55:33 +0800

    create playlist function

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,6 +26,21 @@ function openPage(url) {
 	history.pushState(null, null, url);
 }
 
+function createPlaylist(username) {
+	var alert = prompt("Please enter the name of your playlist");
+
+	if(alert != null) {
+
+		$.post("includes/handlers/ajax/createPlaylist.php", { name: alert, username: username })
+		.done(function() {
+			//do something when ajax returns
+			openPage("yourMusic.php");
+		});
+
+	}
+
+}
+
 
 function formatTime(seconds) {
 	var time = Math.round(seconds);
