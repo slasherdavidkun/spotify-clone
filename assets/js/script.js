203763eefb78272b66f0d147725d46a3c1010d56
slasherdavidkun <slasherdavidkun@gmail.com>
2020-05-06 19:30:20 +0800

    update password js

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,7 +45,25 @@ function updateEmail(emailClass) {
 
 	$.post("includes/handlers/ajax/updateEmail.php", { email: emailValue, username: userLoggedIn})
 	.done(function(response) {
-		$("." + emailClass).nextUntil(".message").text(response);
+		$("." + emailClass).nextAll(".message").text(response);
+	})
+
+
+}
+
+function updatePassword(oldPasswordClass, newPasswordClass1, newPasswordClass2) {
+	var oldPassword = $("." + oldPasswordClass).val();
+	var newPassword1 = $("." + newPasswordClass1).val();
+	var newPassword2 = $("." + newPasswordClass2).val();
+
+	$.post("includes/handlers/ajax/updatePassword.php", 
+		{ oldPassword: oldPassword,
+			newPassword1: newPassword1,
+			newPassword2: newPassword2, 
+			username: userLoggedIn})
+
+	.done(function(response) {
+		$("." + oldPasswordClass).nextAll(".message").text(response);
 	})
 
 
