221f7277ee77e9a6bdbdb4c475fbe583cbf33933
slasherdavidkun <slasherdavidkun@gmail.com>
2020-05-06 18:15:06 +0800

    play button bug

--- includes/nowPlayingBar.php
+++ includes/nowPlayingBar.php
@@ -180,11 +180,12 @@ function setTrack(trackId, newPlaylist, play) {
 
 
 		audioElement.setTrack(track);
+
+		if(play == true) {
+			playSong();
+		}
 	});
 
-	if(play == true) {
-		audioElement.play();
-	}
 }
 
 function playSong() {
