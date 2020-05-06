851d3e71207f47ac1300e50c0ed08b121904b20b
slasherdavidkun <slasherdavidkun@gmail.com>
2020-05-06 18:58:35 +0800

    playlist class

--- yourMusic.php
+++ yourMusic.php
@@ -24,6 +24,8 @@ include("includes/includedFiles.php");
 
 			while($row = mysqli_fetch_array($playlistsQuery)) {
 
+				$playlist = new Playlist($con, $row);
+
 				echo "<div class='gridViewItem'>
 
 						<div class='playlistImage'>
@@ -31,7 +33,7 @@ include("includes/includedFiles.php");
 						</div>
 						
 						<div class='gridViewInfo'>"
-							. $row['name'] .
+							. $playlist->getName() .
 						"</div>
 
 					</div>";
