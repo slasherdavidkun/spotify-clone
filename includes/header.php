7ccc7cc5baa57c51aca3951a95a820e37ac6a626
slasherdavidkun <slasherdavidkun@gmail.com>
2020-05-06 17:42:36 +0800

    intro audio

--- includes/header.php
+++ includes/header.php
@@ -20,9 +20,20 @@ else {
 	<title>Welcome to Slotify!</title>
 
 	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+
+	<script src="assets/js/script.js"></script>
 </head>
 
 <body>
+
+
+
+	<script>
+	var audioElement = new Audio();
+	audioElement.setTrack("assets/music/bensound-acousticbreeze.mp3");
+	audioElement.audio.play();
+
+	</script>
 
 	<div id="mainContainer">
 
