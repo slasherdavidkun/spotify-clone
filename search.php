2e32c6bd263aae443d3c4007ccf5815162f8a42e
slasherdavidkun <slasherdavidkun@gmail.com>
2020-05-06 18:47:58 +0800

    search timeout

--- search.php
+++ search.php
@@ -12,6 +12,28 @@ else {
 <div class="searchContainer">
 
 	<h4>Search for an artist, album or song</h4>
-	<input type="text" class="searchInput" value="<?php echo $term; ?>" placeholder="Start typing...">
+	<input type="text" class="searchInput" value="<?php echo $term; ?>" placeholder="Start typing..." onfocus="this.value = this.value">
 
 </div>
+
+<script>
+
+$(".searchInput").focus();
+
+$(function() {
+	var timer;
+
+	$(".searchInput").keyup(function() {
+		clearTimeout(timer);
+
+		timer = setTimeout(function() {
+			var val = $(".searchInput").val();
+			openPage("search.php?term=" + val);
+		}, 2000);
+
+	})
+
+
+})
+
+</script>
