490dc27958a1c6eaf6b0bcf3de4b979286c905ba
slasherdavidkun <slasherdavidkun@gmail.com>
2020-05-06 18:00:58 +0800

    song ended event

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,6 +33,10 @@ function Audio() {
 	this.currentlyPlaying;
 	this.audio = document.createElement('audio');
 
+	this.audio.addEventListener("ended", function() {
+		nextSong();
+	});
+
 	this.audio.addEventListener("canplay", function() {
 		//'this' refers to the object that the event was called on
 		var duration = formatTime(this.duration);
