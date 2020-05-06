cceb04a37c9d4f0ed701962fe105238280d3580e
slasherdavidkun <slasherdavidkun@gmail.com>
2020-05-06 18:53:32 +0800

    playlist table

--- search.php
+++ search.php
@@ -21,8 +21,7 @@ else {
 $(".searchInput").focus();
 
 $(function() {
-	var timer;
-
+	
 	$(".searchInput").keyup(function() {
 		clearTimeout(timer);
 
@@ -38,6 +37,7 @@ $(function() {
 
 </script>
 
+<?php if($term == "") exit(); ?>
 
 <div class="tracklistContainer borderBottom">
 	<h2>SONGS</h2>
@@ -115,6 +115,7 @@ $(function() {
 	}
 
 	while($row = mysqli_fetch_array($artistsQuery)) {
+
 		$artistFound = new Artist($con, $row['id']);
 
 		echo "<div class='searchResultRow'>
@@ -133,6 +134,35 @@ $(function() {
 	}
 
 
+	?>
+
+</div>
+
+<div class="gridViewContainer">
+	<h2>ALBUMS</h2>
+	<?php
+		$albumQuery = mysqli_query($con, "SELECT * FROM albums WHERE title LIKE '$term%' LIMIT 10");
+
+		if(mysqli_num_rows($albumQuery) == 0) {
+			echo "<span class='noResults'>No albums found matching " . $term . "</span>";
+		}
+
+		while($row = mysqli_fetch_array($albumQuery)) {
+
+			echo "<div class='gridViewItem'>
+					<span role='link' tabindex='0' onclick='openPage(\"album.php?id=" . $row['id'] . "\")'>
+						<img src='" . $row['artworkPath'] . "'>
+
+						<div class='gridViewInfo'>"
+							. $row['title'] .
+						"</div>
+					</span>
+
+				</div>";
+
+
+
+		}
 	?>
 
 </div>
